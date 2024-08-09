c2d3b6304fc6419e52a7f0f6ed18ba88b64d3408
vivianele <anhlv.fpt@gmail.com>
2024-08-09 19:53:55 +0200

    add data

--- Generate_data/utils.py
+++ Generate_data/utils.py
@@ -2,13 +2,13 @@ import gurobipy as gp
 from gurobipy import GRB
 import pandas as pd
 import networkx as nx
-from itertools import chain
 from itertools import islice
 import random
 import pickle
 import time
-import torch
 import numpy as np
+from tqdm.notebook import tqdm
+from collections import defaultdict
 
 def generate_gridNet(dim1, dim2, file_name, draw=True, target_links=80):
     G, pos = create_Grid_Net(dim1, dim2)
@@ -90,7 +90,8 @@ def readNet(fileN) :
     
     nodes = set(list(net['init_node'])+list(net['term_node']))
     
-    links = int(net.shape[0])
+    # links = int(net.shape[0])
+    links = 76
     cap = [0 for i in range(links)]
     t0 = [0 for i in range(links)]
     alpha = [0 for i in range(links)]
@@ -109,18 +110,6 @@ def readNet(fileN) :
         i = i + 1
     return net, nodes, links, cap, t0, alpha, beta, lengths
 
-def get_origDest(OD_demand) : 
-    orig = []
-    dest = []
-    for o,d in OD_demand.keys() :
-        if o not in orig :
-            orig.append(o)
-        if d not in dest :
-            dest.append(o)
-    return orig, dest
-
-
-
 def k_shortest_paths(G, source, target, k):
     try : 
         paths = list(islice(nx.shortest_simple_paths(G, source, target, weight="free_flow_time"), k))
@@ -132,144 +121,15 @@ def k_shortest_paths(G, source, target, k):
 def transform_paths(network, paths) : # transform node path to edge path
     paths_OD = []
     for path in paths:
-        pathEdges = []
+        pathEdges = [] # list of links in each path
         for i in range(len(path)-1):
             mask = (network['init_node']==path[i]) & (network['term_node']==path[i+1])
-            pathEdges.append(network.index[mask].tolist()[0])   
+            pathEdges.append(network.loc[mask, 'link_id'].values[0])   
+            # pathEdges.append(network.index[mask].tolist()[0])   
         paths_OD.append(pathEdges)
     return paths_OD
 
-
-# def find_paths_new(network, OD_Matrix,kk) :
-#     netG = nx.from_pandas_edgelist(network,source='init_node',target='term_node',edge_attr='free_flow_time',create_using=nx.DiGraph())
-#     paths = {}
-#     paths_N = {}
-#     dict_o = {}
-#     dict_d = {}
-#     for key in OD_Matrix.keys():
-#         paths_OD_N = []
-#         o,d = key[0],key[1]
-#         if o in dict_o.keys() :
-#             for v1, v2 in dict_o[o].items() :
-#                 if d<v1 :
-#                     for v3 in v2 :
-#                         if d in v3 :
-#                             p = v3[0:v3.index(d)+1]
-#                             if p not in paths_OD_N :
-#                                 paths_OD_N.append(p)
-#                             if len(paths_OD_N) == kk :
-#                                 paths[(o,d)]=transform_paths(network, paths_OD_N)
-#                                 paths_N[(o,d)]=paths_OD_N
-#                                 dict_o[o].update({d:paths_OD_N})
-#                                 dict_d[d]={o:paths_OD_N}
-#                                 break
-#                 if len(paths_OD_N) == kk :
-#                         break
-#         if d in dict_d.keys() :
-#             for v1, v2 in dict_d[d].items() :
-#                 if o>v1 :
-#                     for v3 in v2 :
-#                         if o in v3 :
-#                             p = v3[v3.index(d):len(v3)+1]
-#                             if p not in paths_OD_N :
-#                                 paths_OD_N.append(p)
-#                             if len(paths_OD_N) == kk :
-#                                 paths[(o,d)]=transform_paths(network, paths_OD_N)
-#                                 paths_N[(o,d)]=paths_OD_N
-#                                 dict_o[o]={d:paths_OD_N}
-#                                 dict_d[d].update({o:paths_OD_N})
-#                                 break
-#                 if len(paths_OD_N) == kk :
-#                         break
-#         if (o,d) not in paths.keys() : 
-#             try : 
-#                 p = k_shortest_paths(netG,o,d,kk)
-#                 paths[(o,d)]=transform_paths(network, p)
-#                 paths_N[(o,d)]=p
-#                 dict_o[o] = {d:p}
-#                 dict_d[d] = {o:p}
-#             except :
-#                 paths[(o,d)] = []
-#                 paths_N[(o,d)]=[]
-    
-#     return paths, paths_N
-
-
-def find_paths(network, OD_Matrix,k) :
-    # print(network)
-    netG = nx.from_pandas_edgelist(network,source='init_node',target='term_node',edge_attr='free_flow_time',create_using=nx.DiGraph())
-    # print("NetG: ", netG)
-    # nx.draw(netG)
-    paths = {}
-    paths_N = {}
-    for key in OD_Matrix.keys():
-        paths_OD = []
-        o,d = key[0],key[1]
-        try : 
-            # print(k)
-            p = k_shortest_paths(netG,o,d,k)
-            paths_OD = transform_paths(network, p)
-            paths[(o,d)]=paths_OD
-            paths_N[(o,d)]=p
-        except :
-            paths[(o,d)] = []
-            paths_N[(o,d)]=[]
-            
-    return paths, paths_N
-
-def mergeDictionary(dict_1, dict_2):
-    dict_3 = {**dict_1, **dict_2}
-    for key, value in dict_3.items():
-        if key in dict_1 and key in dict_2:
-            dict_3[key] = value + dict_1[key]
-    return dict_3
-
-def translate(nodes, OD_demand) :
-    Q = [int(value) for value in OD_demand.values()]
-    OD = len(OD_demand)
-    O_D = [ [0 for i in nodes] for n in range(OD)]
-    n= 0
-    
-    for key in OD_demand.keys() :
-        for i in nodes : 
-            if i==key[0] : #origin node
-                O_D[n][i-1] = -1
-            if i==key[1] : #destination node
-                O_D[n][i-1] = 1
-        n = n + 1
-    
-    return Q, OD, O_D
-
-def create_Adj(net_df, links, nodes): 
-    # links: number of links in the network (80, 75, 70, 65)
-    # nodes: a list of all nodes (from 1 to 25)
-    # Create adj matrix shape 25x80 for all scenarios
-    Adj = [ [0 for i in range(76)] for n in nodes]
-    for n in nodes :
-        init = net_df[net_df['init_node']==n]
-        for j in init['link_id'].values : 
-            Adj[n-1][j] = -1
-        
-        term = net_df[net_df['term_node']==n]
-        for j in term['link_id'].values : 
-            Adj[n-1][j] = 1
-    return Adj
-
-def create_delta(links, paths, od_matrix) :
-    delta = [[[0 for i in range(links)] for p in paths[k]] for k in od_matrix.keys()]
-
-    kk = 0
-    for k in od_matrix.keys() : 
-        value = paths[k]
-        pp = 0
-        for p in value : # p is a list of links
-            for j in p : 
-                delta[kk][pp][j]=1
-            pp += 1
-        kk += 1
-    return delta
-
-def generate_Random_OD_matrix(numuyber_OD, OD_pairs) : 
+def generate_Random_OD_matrix(number_OD, OD_pairs) : 
     max_dem = 300
     Matrix = {}
     total_demand = 0
@@ -351,8 +211,87 @@ def fuse_stats(dict1, dict2) :
                     dict1[k].append(vv)
     return dict1
 
+def mergeDictionary(dict_1, dict_2):
+    dict_3 = {**dict_1, **dict_2}
+    for key, value in dict_3.items():
+        if key in dict_1 and key in dict_2:
+            dict_3[key] = value + dict_1[key]
+    return dict_3
+
+def get_origDest(OD_demand) : 
+    orig = []
+    dest = []
+    for o,d in OD_demand.keys() :
+        if o not in orig :
+            orig.append(o)
+        if d not in dest :
+            dest.append(o)
+    return orig, dest
+
+def find_paths(network, OD_Matrix,k) :
+    netG = nx.from_pandas_edgelist(network,source='init_node',target='term_node',edge_attr='free_flow_time',create_using=nx.DiGraph())
+    paths = {}
+    paths_N = {}
+    for key in OD_Matrix.keys():
+        paths_OD = []
+        o,d = key[0],key[1]
+        try : 
+            p = k_shortest_paths(netG,o,d,k)
+            paths_OD = transform_paths(network, p) # paths_OD is list of feasible path set, each path includes list of links
+            paths[(o,d)]=paths_OD
+            paths_N[(o,d)]=p
+        except :
+            paths[(o,d)] = []
+            paths_N[(o,d)]=[]
+            
+    return paths, paths_N
+
+def translate(nodes, OD_demand) :
+    Q = [int(value) for value in OD_demand.values()]
+    OD = len(OD_demand)
+    O_D = [ [0 for i in nodes] for n in range(OD)]
+    n= 0
+    
+    for key in OD_demand.keys() :
+        for i in nodes : 
+            if i==key[0] : #origin node
+                O_D[n][i-1] = -1
+            if i==key[1] : #destination node
+                O_D[n][i-1] = 1
+        n = n + 1
+    
+    return Q, OD, O_D
+
+def create_Adj(net_df, links, nodes): 
+    # links: number of links in the network (80, 75, 70, 65)
+    # nodes: a list of all nodes (from 1 to 25)
+    # Create adj matrix shape 25x80 for all scenarios
+    Adj = [ [0 for i in range(links)] for n in nodes]
+    for n in nodes :
+        init = net_df[net_df['init_node']==n]
+        for j in init['link_id'].values : 
+            Adj[n-1][j] = -1
+        
+        term = net_df[net_df['term_node']==n]
+        for j in term['link_id'].values : 
+            Adj[n-1][j] = 1
+    return Adj
+
+def create_delta(links, paths, od_matrix) :
+    delta = [[[0 for i in range(links)] for p in paths[k]] for k in od_matrix.keys()]
+
+    kk = 0
+    for k in od_matrix.keys() : 
+        value = paths[k] # get all paths of OD pair k
+        pp = 0
+        for p in value : # iterate each path of pair k
+            for j in p : # iterate each link in path p
+                delta[kk][pp][j]=1
+            pp += 1
+        kk += 1
+    return delta
+
 def get_data(Network, Nodes, links, cap, fft, alpha, beta, lengths, OD_mat) : 
-    # print(Network)
     num_paths = 3
     O,D = get_origDest(OD_mat)
     paths, paths_N = find_paths(Network, OD_mat, num_paths)
@@ -377,6 +316,31 @@ def get_data(Network, Nodes, links, cap, fft, alpha, beta, lengths, OD_mat) :
     #segments_p = segments.difference({0})    
     data = {'network' :Network, 'demand' :OD_mat, 'nodes':Nodes,'links':links,'orig':O,'dest':D,'fftt':fft,'capacity':cap, 'length': lengths, 'beta':beta,
         'approx':segments,'eta':eta,'paths_link':paths, 'paths_node':paths_N, 'delta':delta,'alpha':alpha, 'Adjacency_matrix' : Adj}
+    return data, Q, OD, O_D,n
+
+def get_data_N(Network, Nodes, links, cap, fft, alpha, beta, lengths, OD_mat, paths) : 
+    # print("No of links: ", links)
+    O,D = get_origDest(OD_mat)
+    Q, OD, O_D = translate(Nodes, OD_mat)
+    Adj = create_Adj(Network, links, Nodes)
+    delta = create_delta(links, paths, OD_mat)
+    n = [ len(paths[h]) for h in OD_mat.keys() ]
+    ### Linearizing variables
+    seg = 1000
+    Mflow = 10e4   
+
+    # define the segments
+    segments = set([i for i in range(0,seg+1)])          
+    eta = [ [ v for v in segments ] for i in range(links) ]    
+    for i in range(links):
+        cnt = 0
+        step = Mflow/seg
+        for v in segments:
+            eta[i][v] = cnt*step
+            cnt += 1  
+  
+    data = {'network' :Network, 'demand' :OD_mat, 'nodes':Nodes,'links':links,'orig':O,'dest':D,'fftt':fft,'capacity':cap, 'length': lengths, 'beta':beta,
+        'approx':segments,'eta':eta,'paths_link':paths, 'delta':delta,'alpha':alpha, 'Adjacency_matrix' : Adj}
     return data, Q, OD, O_D,n
 
 def TA_UE(data, n, OD, Q):
@@ -427,58 +391,77 @@ def TA_UE(data, n, OD, Q):
 
     return flows, linkss, model.Status, t2-t1
 
-def convert_DemandtoTensor(OD_demand, nodes) :
-    matrix = [ [0 for n in nodes] for n in nodes]
-    for k,v in OD_demand.items() :
-        o,d = k
-        matrix[o-1][d-1] = v     # matrix_index(node1) = 0 => node1 - 1 
-    return  torch.tensor([matrix], dtype=torch.float32)
+def read_file(filename):
+  with open(filename, "rb") as file:
+      stat = pickle.load(file)
+      file.close()
+  return stat
 
-def getLinks_embedding(data) :
-    links = []
-    for i in range(data['links']) :
-        li = []
-        li.append(data['fftt'][i])
-        li.append(data['capacity'][i])
-        li.append(data['length'][i])
-        li.append(data['beta'][i]) 
-        li.append(data['alpha'][i])
-        links.append(li)
-    return torch.tensor([links], dtype=torch.float32)
+# This function get all feasible paths of all OD pairs.
+# Each OD pair has 3 paths
+# From this origin path set, when remove any link, we will remove the path containing that link, other paths keep no change.
+def get_full_paths(demand_file, net_file, path_num):
+    stat = read_file(demand_file)
+    Network = pd.read_csv(net_file,delimiter='\t',skiprows=8)
 
-def getOutputEncoding(dim1, dim2, max_num_paths, OD_demand, path_flows) : 
-    Full_OD_pairs = get_fullOD_pairs(dim1, dim2)
-    output_size = len(Full_OD_pairs)*max_num_paths
-    output_tensor = torch.zeros([1, output_size], dtype=torch.float32)
-    i = 0
-    for k in OD_demand.keys() :
-        od_ind = Full_OD_pairs.index(k)*max_num_paths
-        flows = path_flows[i]
-        j = 0
-        for f in flows :
-            output_tensor[0,od_ind+j] = f
-            j += 1
-        i += 1
-    return output_tensor
+    path_set = set()
+    pair_path = defaultdict(list)
+    for OD_matrix in tqdm(stat[:10]) :
+        paths, paths_N = find_paths(Network, OD_matrix, path_num)
+        path_set.add(p for path in paths.values() for p in path)
+        for k, v in paths.items():
+            pair_path[k] = [tuple(p) for p in v]
+    pair_path = dict(pair_path)
 
-def create_datapoint(file_name,dim1, dim2, max_num_paths ) :
-    file = open(file_name, "rb")
-    stat = pickle.load(file)
-    file.close()
-    Nodes =  stat['data']['nodes']
-    OD_demand = stat['data']['demand']
+    path_set = {tuple(p) for path in path_set for p in path}
+    path_set_dict = {v: k for k, v in enumerate(path_set, start=1)}
+    return path_set_dict, pair_path
+
+def solve_UE(net_file, demand_file, pair_path, output_file, to_solve):
+    stat = read_file(demand_file)
+    Network, Nodes, links, cap, fft, alpha, beta, lengths = readNet(net_file)
+
+    time = 0
+    for OD_matrix in tqdm(stat[:to_solve]):
+        paths = {k: (pair_path[k][:3] if len(pair_path[k]) >= 3 else pair_path[k]) for k in OD_matrix.keys()}
+        data, Q, OD, O_D,n = get_data_N(Network, Nodes, links, cap, fft, alpha, beta, lengths, OD_matrix, paths)
+        flows, li, status, sol_time = TA_UE(data, n, OD, Q)
+        if status ==2 : # if model solved to optimality => save data, flows, links, status, sol_time
+                dataa = {'data' : data, 'path_flow' : flows, 'link_flow' : li, 'model_status': status, 'solving_time':sol_time}
+                file_data = open(output_file+str(time), "wb")
+                pickle.dump(dataa , file_data)
+                file_data.close()
+                time += 1
+                print("Finish solving file ", time)
+        else : 
+            print("Error ! Networks details :")
+            print(OD_matrix)
+            print("*****************************")
+
+# This function remove the link in the feasible path when that link is removed in the network
+def remove_links_from_path(pair_path, remove_ids):
+    remove_ids = set(remove_ids)
+    new_dict = defaultdict(list)
+    for key, value in pair_path.items():
+        v = [p for p in value if not any(link in p for link in remove_ids)]
+        new_dict[key] = v      
+    return dict(new_dict)
+
+def remove_links_from_tntp(input_file, output_file, remove_ids):
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+    remove_ids = set(remove_ids)
+
+    modified_lines = []
+    for line in lines:
+        parts = line.split()
+        if parts and parts[0].isdigit() and int(parts[0]) in remove_ids:
+            # Replace all values from column 2 onward with '0'
+            parts[1:] = ['0'] * (len(parts) - 2)
+            modified_line = '\t' + '\t'.join(parts) + '\t;\n'
+            modified_lines.append(modified_line)
+        else:
+            modified_lines.append(line)
     
-    T_demand = convert_DemandtoTensor(OD_demand, Nodes)
-    T_Adj = torch.tensor( [stat['data']['Adjacency_matrix']], dtype=torch.float32 )  # tensor of adjacency matrix
-    T_links = getLinks_embedding(stat['data'])
-    T_path_flows = getOutputEncoding(dim1, dim2, max_num_paths, OD_demand, stat['path_flow']) # tensor of path flows
-
-    # need to come up with a better way
-    T_demand = torch.flatten(T_demand, start_dim=1)
-    T_Adj = torch.flatten(T_Adj, start_dim=1)
-    T_links = torch.flatten(T_links, start_dim=1)
-    T_path_flows = torch.flatten(T_path_flows, start_dim=1)
-    return torch.cat((T_demand,T_Adj,T_links),1) , T_path_flows  # X, Y
-
-def flatten_chain(matrix):
-    return list(chain.from_iterable(matrix))
+    with open(output_file, 'w') as file:
+        file.writelines(modified_lines)
