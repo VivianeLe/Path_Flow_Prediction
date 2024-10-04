711f2c50a510c02565a9e0c5043965ab9c91e027
vivianele <anhlv.fpt@gmail.com>
2024-10-04 23:56:31 +0200

    test embedding

--- Generate_data/utils.py
+++ Generate_data/utils.py
@@ -1,3 +1,4 @@
+from fcntl import F_GETLK
 import gurobipy as gp
 from gurobipy import GRB
 import pandas as pd
@@ -9,6 +10,7 @@ import time
 import numpy as np
 from tqdm.notebook import tqdm
 from collections import defaultdict
+import ast
 
 def generate_gridNet(dim1, dim2, file_name, draw=True, target_links=80):
     G, pos = create_Grid_Net(dim1, dim2)
@@ -85,20 +87,42 @@ def convert_net_to_file(net, file_name,labels_map) :
             f.write("\n")
             link_id += 1
 
+# def readNet(fileN) : 
+#     net = pd.read_csv(fileN,delimiter='\t',skiprows=8)
+    
+#     nodes = set(list(net['init_node'])+list(net['term_node']))
+    
+#     links = int(net.shape[0])
+#     cap = [0 for i in range(links)]
+#     t0 = [[0 for j in range(2)] for i in range(links)]
+#     alpha = [0 for i in range(links)]
+#     beta = [0 for i in range(links)]
+#     lengths = [0 for i in range(links)]
+    
+#     net['link_id'] = net.index
+    
+#     for i, (capacityi,fftti,alphai,betai,leni) in enumerate(zip(net['capacity'],net['fft'],net['power'],net['b'], net['length'])):
+#         fftti = ast.literal_eval(fftti)
+#         cap[i] = capacityi
+#         for j in range(2):
+#             t0[i][j] = fftti[j]
+#         alpha[i] = alphai
+#         beta[i] = betai
+#         lengths[i] = leni
+
+#     return net, nodes, links, cap, t0, alpha, beta, lengths
+
 def readNet(fileN) : 
     net = pd.read_csv(fileN,delimiter='\t',skiprows=8)
     
     nodes = set(list(net['init_node'])+list(net['term_node']))
     
-    # links = int(net.shape[0])
-    links = 76
+    links = int(net.shape[0])
     cap = [0 for i in range(links)]
     t0 = [0 for i in range(links)]
     alpha = [0 for i in range(links)]
     beta = [0 for i in range(links)]
     lengths = [0 for i in range(links)]
-    
-    # net['link_id'] = net.index
     
     i = 0
     for capacityi,fftti,alphai,betai,leni in zip(net['capacity'],net['free_flow_time'],net['power'],net['b'], net['length']):
@@ -158,8 +182,9 @@ def generate_OD_demand(num_nodes, min_demand, max_demand, num_pairs):
 
     # Assign random demand values to each OD pair
     for origin, destination in pairs:
-        demand = random.randint(min_demand, max_demand)
-        od_demand[(origin, destination)] = demand
+        demand_c = random.randint(min_demand, max_demand)
+        demand_t = int(demand_c/2)
+        od_demand[(origin, destination)] = [demand_c, demand_t]
     return od_demand
 
 def generate_Random_ODs(dim1, dim2, nb_entries,origins, dest,OD_pairs,file_name) : 
@@ -247,7 +272,7 @@ def find_paths(network, OD_Matrix,k) :
     return paths, paths_N
 
 def translate(nodes, OD_demand) :
-    Q = [int(value) for value in OD_demand.values()]
+    Q = [value for value in OD_demand.values()]
     OD = len(OD_demand)
     O_D = [ [0 for i in nodes] for n in range(OD)]
     n= 0
@@ -291,58 +316,160 @@ def create_delta(links, paths, od_matrix) :
         kk += 1
     return delta
 
-def get_data(Network, Nodes, links, cap, fft, alpha, beta, lengths, OD_mat) : 
-    num_paths = 3
+def get_data(Network, Nodes, links, cap, fft, alpha, beta, lengths, OD_mat, paths) : 
     O,D = get_origDest(OD_mat)
-    paths, paths_N = find_paths(Network, OD_mat, num_paths)
     Q, OD, O_D = translate(Nodes, OD_mat)
     Adj = create_Adj(Network, links, Nodes)
-    delta = create_delta(links, paths, OD_mat)
+    delta = create_delta(links, paths, OD_mat) #Adj of OD pair - path - link
     n = [ len(paths[h]) for h in OD_mat.keys() ]
     ### Linearizing variables
     seg = 1000
-    Mflow = 10e4   
+    Mflow = 10e4
 
     # define the segments
     segments = set([i for i in range(0,seg+1)])          
-    eta = [ [ v for v in segments ] for i in range(links) ]    
+    eta = [ [ v for v in segments ] for i in range(links) ]  
+    step = Mflow/seg  
     for i in range(links):
         cnt = 0
-        step = Mflow/seg
         #step = cap[i]/seg
         for v in segments:
             eta[i][v] = cnt*step
             cnt += 1  
     #segments_p = segments.difference({0})    
     data = {'network' :Network, 'demand' :OD_mat, 'nodes':Nodes,'links':links,'orig':O,'dest':D,'fftt':fft,'capacity':cap, 'length': lengths, 'beta':beta,
-        'approx':segments,'eta':eta,'paths_link':paths, 'paths_node':paths_N, 'delta':delta,'alpha':alpha, 'Adjacency_matrix' : Adj}
-    return data, Q, OD, O_D,n
-
-def get_data_N(Network, Nodes, links, cap, fft, alpha, beta, lengths, OD_mat, paths) : 
-    # print("No of links: ", links)
-    O,D = get_origDest(OD_mat)
-    Q, OD, O_D = translate(Nodes, OD_mat)
-    Adj = create_Adj(Network, links, Nodes)
-    delta = create_delta(links, paths, OD_mat)
-    n = [ len(paths[h]) for h in OD_mat.keys() ]
-    ### Linearizing variables
-    seg = 1000
-    Mflow = 10e4   
-
-    # define the segments
-    segments = set([i for i in range(0,seg+1)])          
-    eta = [ [ v for v in segments ] for i in range(links) ]    
-    for i in range(links):
-        cnt = 0
-        step = Mflow/seg
-        for v in segments:
-            eta[i][v] = cnt*step
-            cnt += 1  
-  
-    data = {'network' :Network, 'demand' :OD_mat, 'nodes':Nodes,'links':links,'orig':O,'dest':D,'fftt':fft,'capacity':cap, 'length': lengths, 'beta':beta,
         'approx':segments,'eta':eta,'paths_link':paths, 'delta':delta,'alpha':alpha, 'Adjacency_matrix' : Adj}
     return data, Q, OD, O_D,n
 
+# def get_data_N(Network, Nodes, links, cap, fft, alpha, beta, lengths, OD_mat, paths) : 
+#     # print("No of links: ", links)
+#     O,D = get_origDest(OD_mat)
+#     Q, OD, O_D = translate(Nodes, OD_mat)
+#     Adj = create_Adj(Network, links, Nodes)
+#     delta = create_delta(links, paths, OD_mat)
+#     n = [ len(paths[h]) for h in OD_mat.keys() ] # number of path for each OD pair 
+#     ### Linearizing variables
+#     seg = 1000
+#     Mflow = 10e4   
+
+#     # define the segments
+#     segments = set([i for i in range(0,seg+1)])          
+#     eta = [[[ v for j in range(2)] for v in segments] for i in range(links)]    
+#     for i in range(links):
+#         cnt = 0
+#         step = Mflow/seg
+#         for v in segments:
+#             for j in range(2):
+#                 eta[i][v][j] = cnt*step
+#             cnt += 1  
+  
+#     data = {'network' :Network, 'demand' :OD_mat, 'nodes':Nodes,'links':links,'orig':O,'dest':D,'fftt':fft, 'capacity':cap, 'length': lengths, 'beta':beta,
+#         'approx':segments,'eta':eta,'paths_link':paths, 'delta':delta,'alpha':alpha, 'Adjacency_matrix' : Adj}
+#     return data, Q, OD, O_D,n
+
+#################### BRUE SOLVER ##########################
+def BRUE(data, n, OD, Q):
+    model = gp.Model("BRUE")
+    model.setParam("OutputFlag", 1)
+    model.setParam("LogFile", "gurobi_log.txt")
+    model.setParam("NumericFocus", 3)
+    # model.setParam("Method", 2)  # Barrier method
+
+    a = data['links']
+    segments = data['approx']
+    t0 = data['fftt']
+    eta = data['eta']
+    alpha = data['alpha'] #4
+    beta = data['beta'] #0.15
+    sigma = data['delta'] # adj of od pair - path - link 
+    cap = data['capacity']
+    segments_p = segments.difference({0})
+    M = 1e3
+    
+    x = [model.addVar(vtype=GRB.CONTINUOUS) for j in range(a)] # link flow
+    x4 = [model.addVar(vtype=GRB.CONTINUOUS) for j in range(a)] # x^4
+    link_cost = [model.addVar(vtype=GRB.CONTINUOUS) for j in range(a)]
+    f = [ [model.addVar(vtype=GRB.CONTINUOUS) for i in range(n[k])] for k in range(OD)] # path flow
+    path_cost = [ [model.addVar(vtype=GRB.CONTINUOUS) for i in range(n[k])] for k in range(OD)]
+    
+    ll = [ [model.addVar(vtype=GRB.CONTINUOUS) for l in segments] for i in range(a)]
+    lr = [ [model.addVar(vtype=GRB.CONTINUOUS) for l in segments] for i in range(a)]
+
+    y = [ [model.addVar(vtype=GRB.CONTINUOUS) for i in range(n[k])] for k in range(OD)] # BINARY VAR
+    min_path_cost = [model.addVar(vtype=GRB.CONTINUOUS, name=f"min_path_cost_{k}") for k in range(OD)]
+
+    for i in range(a):
+        model.addConstr(x[i] == sum( sum(f[k][p]*sigma[k][p][i] for p in range(n[k])) for k in range(OD)), "link-path%d" %i)
+        model.addConstr(x[i] == sum(ll[i][v]*eta[i][v-1] + lr[i][v]*eta[i][v] for v in segments_p), "Approx1%d" %i)
+        model.addConstr(sum(ll[i][v] + lr[i][v] for v in segments_p) == 1, "Approx2%d" %i)
+        model.addConstr(x[i] >= 0, "integrality_x%d" %i)  
+
+        for ss in segments:
+            model.addConstr(ll[i][ss] >= 0, "integrality_ll%d%d" %(i,ss))
+            model.addConstr(lr[i][ss] >= 0, "integrality_lr%d%d" %(i,ss))
+        
+        model.addConstr(x4[i] == sum(eta[i][v-1]**alpha[i] * ll[i][v] + eta[i][v]**alpha[i] * lr[i][v] 
+                                                     for v in segments_p))
+
+        model.addConstr(link_cost[i] == t0[i] * (1 + beta[i]/(cap[i]**alpha[i]) * 
+                                                 sum(eta[i][v-1]**alpha[i] * ll[i][v] + eta[i][v]**alpha[i] * lr[i][v] 
+                                                     for v in segments_p)),
+                                                 name=f"link_cost_BPR_{i}")
+
+        # num_segments = 50
+        # breakpoints = np.linspace(0, 10e4, num_segments)  # create 1000 segments from 0 to 10e4
+        # values = t0[i] * (1 + beta[i] * (breakpoints / cap[i]) ** alpha[i])
+        # model.addGenConstrPWL(x[i], link_cost[i], breakpoints.tolist(), values.tolist(), name=f"link_cost_BPR_PWL_{i}")
+
+
+    for k in range(OD) :
+        model.addConstr( sum(f[k][p] for p in range(n[k])) == Q[k] , "FConservation%d" %k ) 
+
+        if (n[k] > 0):
+            model.addConstr(min_path_cost[k] == path_cost[k][0], name=f"init_min_cost_{k}")
+
+        for p in range(n[k]) : 
+            model.addConstr(f[k][p] >= 0, "integrality%d%d" %(k,p))
+
+            model.addConstr(y[k][p] >= 0)
+            model.addConstr(y[k][p] <= 1)
+            model.addConstr(f[k][p] <= M * y[k][p]) # if f[k][i] = 0 then y[k][i] = 0
+            model.addConstr(f[k][p] >= 1e-6 * y[k][p]) # if f[k][i] > 0 then y[k][i] = 1
+            # model.addConstr(f[k][p] * (1-y[k][p]) <= f[k][p])
+            # model.addConstr(f[k][p] * y[k][p] == f[k][p]) # add this one make infeasible
+
+            model.addConstr(path_cost[k][p] == sum(link_cost[i] * sigma[k][p][i] for i in range(a)), "path-cost%d%d" %(k,p)) # satisfied
+            model.addConstr((min_path_cost[k] <= path_cost[k][p]), name=f"min_cost_{k}") # satisfied
+
+            model.addConstr(path_cost[k][p] - min_path_cost[k] <= M* (1-y[k][p]) + min_path_cost[k]*0.05) #BRUE
+            # model.addConstr(M * (path_cost[k][p] - min_path_cost[k]) >= 1-y[k][p])
+        
+    Z = sum(M * y[k][p] for p in range(n[k]) for k in range(OD))
+
+    model.setObjective(Z, GRB.MINIMIZE)
+    t1 = time.time()
+    model.optimize()
+    t2 = time.time()
+    print('model solved in:',t2-t1)
+    
+    if model.Status == GRB.OPTIMAL:
+        flows =  [ [ f[k][p].X  for p in range(n[k])] for k in range(OD)]
+        linkss = [ x[i].X  for i in range(a)]
+        x4 = [ x4[i].X  for i in range(a)]
+        min_cost = [min_path_cost[i].X for i in range(OD)]
+        path_cost = [[path_cost[k][p].X for p in range(n[k])] for k in range(OD)]
+        link_cost = [link_cost[i].X for i in range(a)]
+        return flows, linkss, path_cost, min_cost, link_cost, x4
+    
+    elif model.Status == GRB.INFEASIBLE:
+        print(f"Model is infeasible, status {model.Status}")
+        return None, None, None, None, None
+    
+    else:
+        print(f"Model did not solve to optimality. Status: {model.Status}")
+        return None, None, None, None, None
+
+################ SINGLE CLASS SOLVER ####################
 def TA_UE(data, n, OD, Q):
     model = gp.Model("UE")
     model.setParam("OutputFlag", 0)
@@ -389,7 +516,78 @@ def TA_UE(data, n, OD, Q):
     flows =  [ [ f[k][p].X  for p in range(n[k])] for k in range(OD)]
     linkss = [ x[i].X   for i in range(a)]
 
-    return flows, linkss, model.Status, t2-t1
+    return flows, linkss
+    
+############## MULTI CLASS UE SOLVER ###############
+# def TA_UE(data, n, OD, Q):
+#     # n: number of path of each OD pair
+#     # OD: number of OD pair 
+#     # Q: value of demand 
+#     model = gp.Model("UE")
+#     model.setParam("OutputFlag", 0)
+
+#     a = data['links']
+#     segments = data['approx']
+#     t0 = data['fftt']
+#     eta = data['eta']
+#     alpha = data['alpha']
+#     beta = data['beta']
+#     sigma = data['delta']
+#     cap = data['capacity']
+#     segments_p = segments.difference({0})
+
+#     pi = [[1, 2.5] for i in range(a)]
+    
+#     x = [[model.addVar(vtype=GRB.CONTINUOUS) for j in range(2)]for i in range(a)]
+#     f = [ [[model.addVar(vtype=GRB.CONTINUOUS) for j in range(2)]for i in range(n[k])] for k in range(OD)]
+#     ll = [ [[model.addVar(vtype=GRB.CONTINUOUS) for j in range(2)] for l in segments] for i in range(a)]
+#     lr = [ [[model.addVar(vtype=GRB.CONTINUOUS) for j in range(2)] for l in segments] for i in range(a)]
+
+#     for i in range(a):
+#         # x[i][1] = x[i][1] * 2.5
+#         for j in range(2):
+#             model.addConstr(x[i][j] == sum(sum(f[k][p][j]*sigma[k][p][i] for p in range(n[k])) for k in range(OD)), "link-path%d" %i)
+#             model.addConstr(x[i][j] == sum(ll[i][v][j]*eta[i][v-1][j] + lr[i][v][j]*eta[i][v][j] for v in segments_p), "Approx1%d" %i)
+
+#             model.addConstr(sum(ll[i][v][j] + lr[i][v][j] for v in segments_p) == 1, "Approx2%d" %i)
+#             model.addConstr(x[i][j] >= 0, "integrality_x%d" %i)   
+
+#             for ss in segments:
+#                 model.addConstr(ll[i][ss][j] >= 0, "integrality_ll%d%d" %(i,ss))
+#                 model.addConstr(lr[i][ss][j] >= 0, "integrality_lr%d%d" %(i,ss))
+
+#     for k in range(OD) :
+#         for j in range(2):
+#             model.addConstr( sum(f[k][p][j] for p in range(n[k])) == Q[k][j] , "FConservation%d" %k ) 
+#             for p in range(n[k]) : 
+#                 model.addConstr(f[k][p][j] >= 0, "integrality%d%d" %(k,p))
+    
+#     Z = sum( t0[i][j] * (x[i][j]+1/(alpha[i]+1)*beta[i]*(pi[i][j]**alpha[i])/(cap[i]**alpha[i])*
+#                     sum(eta[i][v-1][j]**(alpha[i]+1)*ll[i][v][j] + eta[i][v][j]**(alpha[i]+1)*lr[i][v][j] for v in segments_p))
+#                     for j in range(2)
+#                     for i in range(a))
+
+#     model.setObjective(Z, GRB.MINIMIZE)
+#     t1 = time.time()
+#     model.optimize()
+#     t2 = time.time()
+#     print('model solved in:',t2-t1)
+    
+#     if model.Status == GRB.OPTIMAL:
+#         flows =  [[[f[k][p][j].X for j in range(2)] for p in range(n[k])] for k in range(OD)]
+#         linkss = [[x[i][j].X for j in range(2)] for i in range(a)]
+#         # linkss = None
+#         return flows, linkss
+    
+#     elif model.Status == GRB.INFEASIBLE:
+#         print(f"Model is infeasible, status {model.Status}")
+#         model.computeIIS()
+#         model.write("model.ilp")
+#         return None, None
+    
+#     else:
+#         print(f"Model did not solve to optimality. Status: {model.Status}")
+#         return None, None
 
 def read_file(filename):
   with open(filename, "rb") as file:
@@ -417,26 +615,23 @@ def get_full_paths(demand_file, net_file, path_num):
     path_set_dict = {v: k for k, v in enumerate(path_set, start=1)}
     return path_set_dict, pair_path
 
-def solve_UE(net_file, demand_file, pair_path, output_file, to_solve):
-    stat = read_file(demand_file)
-    Network, Nodes, links, cap, fft, alpha, beta, lengths = readNet(net_file)
+# def solve_UE(net_file, demand_file, pair_path, output_file, to_solve):
+#     stat = read_file(demand_file)
+#     Network, Nodes, links, cap, fft, alpha, beta, lengths = readNet(net_file)
 
-    time = 0
-    for OD_matrix in tqdm(stat[:to_solve]):
-        paths = {k: (pair_path[k][:3] if len(pair_path[k]) >= 3 else pair_path[k]) for k in OD_matrix.keys()}
-        data, Q, OD, O_D,n = get_data_N(Network, Nodes, links, cap, fft, alpha, beta, lengths, OD_matrix, paths)
-        flows, li, status, sol_time = TA_UE(data, n, OD, Q)
-        if status ==2 : # if model solved to optimality => save data, flows, links, status, sol_time
-                dataa = {'data' : data, 'path_flow' : flows, 'link_flow' : li, 'model_status': status, 'solving_time':sol_time}
-                file_data = open(output_file+str(time), "wb")
-                pickle.dump(dataa , file_data)
-                file_data.close()
-                time += 1
-                print("Finish solving file ", time)
-        else : 
-            print("Error ! Networks details :")
-            print(OD_matrix)
-            print("*****************************")
+#     time = 0
+#     for OD_matrix in tqdm(stat[:to_solve]):
+#         print(time)
+#         paths = {k: (pair_path[k][:3] if len(pair_path[k]) >= 3 else pair_path[k]) for k in OD_matrix.keys()}
+#         data, Q, OD, O_D,n = get_data(Network, Nodes, links, cap, fft, alpha, beta, lengths, OD_matrix, paths)
+#         flows, linkss, path_cost, min_cost, link_cost = BRUE(data, n, OD, Q)
+#         dataa = {'data' : data, 'path_flow' : flows, 'link_flow' : linkss, 'path_cost': path_cost, 'min_cost': min_cost, 'link_cost': link_cost}
+#         # flows, linkss = BRUE(data, n, OD, Q)
+#         # dataa = {'data' : data, 'path_flow' : flows, 'link_flow' : linkss}
+#         file_data = open(output_file+str(time), "wb")
+#         pickle.dump(dataa , file_data)
+#         file_data.close()
+#         time +=1
 
 # This function remove the link in the feasible path when that link is removed in the network
 def remove_links_from_path(pair_path, remove_ids):
